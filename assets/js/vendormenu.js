3c8533b2916d926beca5649c7d777b132080fc5e
unknown <adenijidamilola2@gmail.com>
2020-06-21 14:51:55 +0100

    Made Changes

---
+++ assets/js/vendormenu.js
@@ -0,0 +1,19 @@
+const menulist = [{
+    menuname: "Dashboard",
+    menulink: "index.html"
+},{
+    menuname: "Event",
+    menulink: ""
+},{
+    menuname: "Booked Services",
+    menulink: "Bookedservices.html",
+},{
+    menuname: "Users",
+    menulink: "user.html",
+},{
+    menuname: "Past Events",
+    menulink: "history.html",
+},{
+    menuname: "Create Service",
+    menulink: "createservice.html",
+}],
